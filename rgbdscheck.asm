9b3bb9cbbec2225367b6461670c2c66c9e15f90e
lsmmega <pogmegalsm@outlook.com>
2024-12-09 21:04:14 +0800

    Initial commit

---
+++ rgbdscheck.asm
@@ -0,0 +1,6 @@
+IF !DEF(__RGBDS_MAJOR__) || !DEF(__RGBDS_MINOR__) || !DEF(__RGBDS_PATCH__)
+	fail "pokepale requires rgbds v0.8.0 or newer."
+ENDC
+IF __RGBDS_MAJOR__ == 0 && __RGBDS_MINOR__ <  8
+	fail "pokepale requires rgbds v0.8.0 or newer."
+ENDC
