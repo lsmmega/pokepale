c7e459645bd73377beab5492d1e2a6debc253212
lsmmega <pogmegalsm@outlook.com>
2024-12-22 22:59:28 +0800

    add exp growth rate under tutorial

--- data/growth_rates.asm
+++ data/growth_rates.asm
@@ -18,4 +18,36 @@ GrowthRates:
 	growth_rate 6, 5, -15, 100, 140 ; Medium Slow
 	growth_rate 4, 5,   0,   0,   0 ; Fast
 	growth_rate 5, 4,   0,   0,   0 ; Slow
-	assert_table_length NUM_GROWTH_RATES
+	assert_table_length NUM_GROWTH_RATES - 2 ; exclude Erratic and Fluctuating
+
+ErraticExperience:
+	table_width 3, ErraticExperience
+	for n, 1, MAX_LEVEL + 1
+		if n == 1
+			dt 0
+		elif n < 50
+			dt (n**3 * (100 - n)) / 50
+		elif n < 68
+			dt (n**3 * (150 - n)) / 100
+		elif n < 98
+			dt (n**3 * ((1911 - 10 * n) / 3)) / 500
+		else
+			dt (n**3 * (160 - n)) / 100
+		endc
+	endr
+	assert_table_length MAX_LEVEL
+
+FluctuatingExperience:
+	table_width 3, FluctuatingExperience
+	for n, 1, MAX_LEVEL + 1
+		if n == 1
+			dt 0
+		elif n < 15
+			dt (n**3 * ((n + 1) / 3 + 24)) / 50
+		elif n < 36
+			dt (n**3 * (n + 14)) / 50
+		else
+			dt (n**3 * (n / 2 + 32)) / 50
+		endc
+	endr
+	assert_table_length MAX_LEVEL
