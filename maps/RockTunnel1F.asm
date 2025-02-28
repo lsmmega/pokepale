40933d7d5acd7e7e4e1643ec9a44af653b7c930c
lsmmega <pogmegalsm@outlook.com>
2025-03-01 00:04:26 +0800

    reusable TMs under tutorial

--- maps/RockTunnel1F.asm
+++ maps/RockTunnel1F.asm
@@ -1,6 +1,5 @@
 	object_const_def
-	const ROCKTUNNEL1F_POKE_BALL1
-	const ROCKTUNNEL1F_POKE_BALL2
+	const ROCKTUNNEL1F_POKE_BALL
 
 RockTunnel1F_MapScripts:
 	def_scene_scripts
@@ -9,9 +8,6 @@ RockTunnel1F_MapScripts:
 
 RockTunnel1FElixer:
 	itemball ELIXER
-
-RockTunnel1FTMSteelWing:
-	itemball TM_STEEL_WING
 
 RockTunnel1FHiddenXAccuracy:
 	hiddenitem X_ACCURACY, EVENT_ROCK_TUNNEL_1F_HIDDEN_X_ACCURACY
@@ -38,4 +34,3 @@ RockTunnel1F_MapEvents:
 
 	def_object_events
 	object_event  4, 18, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnel1FElixer, EVENT_ROCK_TUNNEL_1F_ELIXER
-	object_event 10, 15, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, RockTunnel1FTMSteelWing, EVENT_ROCK_TUNNEL_1F_TM_STEEL_WING
