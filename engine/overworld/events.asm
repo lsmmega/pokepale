8de32e9c89bd2dc835b3931abacdf90acf3b0f23
lsmmega <pogmegalsm@outlook.com>
2025-03-01 23:41:40 +0800

    no poison step under tutorial

--- engine/overworld/events.asm
+++ engine/overworld/events.asm
@@ -121,11 +121,6 @@ EnterMap:
 
 	ldh a, [hMapEntryMethod]
 	cp MAPSETUP_RELOADMAP
-	jr nz, .dontresetpoison
-	xor a
-	ld [wPoisonStepCount], a
-.dontresetpoison
-
 	xor a ; end map entry
 	ldh [hMapEntryMethod], a
 	ld a, MAPSTATUS_HANDLE
@@ -877,9 +872,6 @@ CountStep:
 	call DoRepelStep
 	jr c, .doscript
 
-	; Count the step for poison and total steps
-	ld hl, wPoisonStepCount
-	inc [hl]
 	ld hl, wStepCount
 	inc [hl]
 	; Every 256 steps, increase the happiness of all your Pokemon.
@@ -901,18 +893,6 @@ CountStep:
 .skip_egg
 	; Increase the EXP of (both) DayCare Pokemon by 1.
 	farcall DayCareStep
-
-	; Every 4 steps, deal damage to all poisoned Pokemon.
-	ld hl, wPoisonStepCount
-	ld a, [hl]
-	cp 4
-	jr c, .skip_poison
-	ld [hl], 0
-
-	farcall DoPoisonStep
-	jr c, .doscript
-
-.skip_poison
 	farcall DoBikeStep
 
 .done
