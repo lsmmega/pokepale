332a0455c1c53180b100a73a72bc1086f5f44f65
lsmmega <pogmegalsm@outlook.com>
2025-10-08 12:05:18 +0800

    add reserved

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -108,7 +108,7 @@ Moves:
 	move HYPNOSIS,           EFFECT_SLEEP,               0, PSYCHIC_TYPE,  STATUS,    60, 20,   0
 	move MEDITATE,           EFFECT_ATTACK_UP,           0, PSYCHIC_TYPE,  STATUS,   100, 40,   0
 	move AGILITY,            EFFECT_SPEED_UP_2,          0, PSYCHIC_TYPE,  STATUS,   100, 30,   0
-	move QUICK_ATTACK,       EFFECT_PRIORITY_HIT,       40, NORMAL,        PHYSICAL, 100, 30,   0
+	move QUICK_ATTACK,       EFFECT_PRIORITY_HIT,       40, NORMAL,        PHYSICAL, 100, 30,   0 ;reserved
 	move RAGE,               EFFECT_RAGE,               20, NORMAL,        PHYSICAL, 100, 20,   0
 	move TELEPORT,           EFFECT_TELEPORT,            0, PSYCHIC_TYPE,  STATUS,   100, 20,   0
 	move NIGHT_SHADE,        EFFECT_LEVEL_DAMAGE,        1, GHOST,         SPECIAL,  100, 15,   0
